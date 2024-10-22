4da29d6ae60671d56e6750fe0f52806bfb014929
shaima Alhussain <shyma.alhussain@gmail.com>
2024-10-22 11:32:10 +0300

    Initial Commit

---
+++ ReceipsUpdated/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ReceipsUpdated
+//
+//  Created by Shaima Alhussain on 19/04/1446 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ReceipsUpdated/ReceipsUpdatedApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReceipsUpdatedApp.swift
+//  ReceipsUpdated
+//
+//  Created by Shaima Alhussain on 19/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct ReceipsUpdatedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
